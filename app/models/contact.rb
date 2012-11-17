d48017d5343ac878fdd2a4010711e780308b7a44
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-17 00:11:44 -0500

    create association btwn contact and message

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
+  has_many :messages
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
   attr_accessible :content, :user_id
+  belongs_to :contact
 end
