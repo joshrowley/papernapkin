40ee370aaeb493f1b996013d5b522888ecfb0ace
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-17 00:03:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Papernapkin::Application.config.secret_token = '528ec79d73076838c00af007ad7a4fb875c3c44813c7843c3e0ffc691444bb101853231010fa651b48034054ef1825dc6549d3003350acf5e36ba85594d07986'
